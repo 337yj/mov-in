f8c0c6b44ee731ce4a90af0d663272ce34c2f891
337yj <337yje@gmail.com>
2023-03-23 12:33:31 +0900

    fix/components : Fix SearchInput, common Input

--- src/components/Common/Input/index.js
+++ src/components/Common/Input/index.js
@@ -4,14 +4,15 @@ import styles from "./input.module.scss";
 
 // 로그인, 회원가입 input
 // form태그로 감싸주고 Input 사용하기
-const Input = ({ className, label, id, placeholder, ...props }) => {
+const Input = ({ className, label, id, name, placeholder, ...props }) => {
   return (
-    <label htmlFor={id} className={cx(styles.label, className)}>
+    // label이 input을 감싸고 있을 땐 htmlFor안써줘도 됨
+    <label className={cx(styles.label, className)}>
       {/* 추후 사용시 value속성 추가?*/}
       <input
         type="text"
         id={id}
-        name={id}
+        name={name}
         placeholder={placeholder}
         className={styles.input}
         {...props}

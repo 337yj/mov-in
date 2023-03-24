2041250efa0f31fe5f2eba794e315687d2534bab
337yj <337yje@gmail.com>
2023-03-24 11:08:30 +0900

    feat/MyPage : Implement MyPage router

--- src/pages/MyPage/UserInfo/index.js
+++ src/pages/MyPage/UserInfo/index.js
@@ -1,7 +1,12 @@
 import React from "react";
+import styles from "./userInfo.module.scss";
 
 const UserInfo = () => {
-  return <div>유저인포 페이지</div>;
+  return (
+    <section className={styles.wrapper}>
+      <h1 className={styles.title}>회원정보 페이지</h1>
+    </section>
+  );
 };
 
 export default UserInfo;
--- src/router/index.js
+++ src/router/index.js
@@ -9,15 +9,24 @@ import {
   HomePage,
   ComponentPage,
   SearchResultPage,
-  CommetDetailPage,
+  MyPage,
+  UserInfo,
+  Comment,
+  Like,
+  Bookmark,
 } from "../pages";
 
 const route = (
   <Route path="/" element={<Layout />}>
     <Route index element={<HomePage />} />
     <Route path="searchResult" element={<SearchResultPage />} />
+    <Route path="myPage" element={<MyPage />}>
+      <Route path="/myPage/userInfo" element={<UserInfo />} />
+      <Route path="/myPage/comment" element={<Comment />} />
+      <Route path="/myPage/like" element={<Like />} />
+      <Route path="/myPage/bookmark" element={<Bookmark />} />
+    </Route>
     <Route path="components" element={<ComponentPage />} />
-    <Route path="commentDetali" element={<CommetDetailPage />} />
   </Route>
 );
 

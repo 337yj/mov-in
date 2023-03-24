3d50d996e3f794bdc078bc11004f8e1b63d0227b
minjeongraw34 <122338406+minjeongraw34@users.noreply.github.com>
2023-03-24 10:43:46 +0900

    Delete src/pages/DetailPage/Comment/components directory

--- src/pages/DetailPage/Comment/components/index.js
+++
@@ -1,5 +0,0 @@
-export { default as CommentBox } from "./CommentBox";
-export { default as ReplyBox } from "./ReplyBox";
-export { default as LikeCount } from "./likeCount";
-export { default as CommentCount } from "./commnetCount";
-export { default as Profile } from "./profile";

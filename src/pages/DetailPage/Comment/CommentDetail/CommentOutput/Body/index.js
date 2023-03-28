9eaaa8534c4776424a9e66d3b4d9b3eb172a8746
kyoongdev <9898junjun2@gmail.com>
2023-03-28 09:36:59 +0900

    feedback : 2023.03.28

--- src/pages/DetailPage/Comment/CommentDetail/CommentOutput/Body/index.js
+++ src/pages/DetailPage/Comment/CommentDetail/CommentOutput/Body/index.js
@@ -7,9 +7,14 @@ const CommentBody = ({ content, ...props }) => {
   // props type 작성
 
   return (
+    // NOTE: body태그 사용 X -> section / article
     <body className={styles.commentContent}>
       <hr className={styles.horizontalLine} />
+
+      {/* //NOTE: content -> p */}
       <content className={styles.comment}>{content}</content>
+      {/* //NOTE: output -> 어떤 결과를 표기할 때 쓰는 태그 */}
+      {/* //NOTE: div or footer */}
       <output className={styles.icon}>
         {/* 알림 띄우고 수정 모달로 이동 기능 추가*/}
         <IconModify />
--- src/pages/DetailPage/index.js
+++ src/pages/DetailPage/index.js
@@ -3,14 +3,14 @@ import { useParams } from "react-router-dom";
 import styles from "./detail.module.scss";
 
 const Detail = () => {
-const { title } = useParams();
-    return(
-        <main>
-            <section className={styles.wrapper}>
-                this is detail page(temporary)
-            </section>
-        </main>
-    );
-}
+  const { title } = useParams();
+  return (
+    <main>
+      <section className={styles.wrapper}>
+        this is detail page(temporary)
+      </section>
+    </main>
+  );
+};
 
 export default Detail;

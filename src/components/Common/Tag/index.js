70eefa16fdae773049789abf19a9d768e2e90036
naim <rlaskdla97@gmail.com>
2023-03-29 17:58:09 +0900

    fix : modify component

--- src/components/Common/Tag/index.js
+++ src/components/Common/Tag/index.js
@@ -1,2 +1,10 @@
-export { default as NormalTag } from "./NormalTag";
-export { default as TensionTag } from "./TensionTag";
+import cx from "classnames";
+import styles from "./tag.module.scss";
+
+const Tag = ({className, children, type, ...props}) => {
+    return (
+        <button type="submit" className={cx(styles.tag, {[styles.isSelected] : isSelected})}>{children}</button>
+    )
+};
+
+export default Tag;

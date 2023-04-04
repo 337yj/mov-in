caf3a8612702b7d544b9eb324bd1d00847cea944
337yj <337yje@gmail.com>
2023-04-04 19:25:02 +0900

    fix/searchResult : Fix searchResult Page

--- src/components/Common/SearchInput/index.js
+++ src/components/Common/SearchInput/index.js
@@ -5,29 +5,28 @@ import cx from "classnames";
 import styles from "./searchInput.module.scss";
 
 const SearchInput = ({ className, placeholder, ...props }) => {
-  const [searchText, setSearchText] = useState("");
+  const [state, setState] = useState({ keyword: "", results: [] });
   const navigate = useNavigate();
 
-  const onSubmit = (e) => {
-    e.preventDefault();
-    // 검색 결과페이지로 이동
-    navigate(`/searchResult?search=${searchText}`);
-    // `/searchResult/${searchText}`
+  const onChange = (event) => {
+    setState({ ...state, keyword: event.target.value });
+  };
+  const onSubmit = async (event) => {
+    event.preventDefault();
+    onClick();
   };
 
-  // 검색값
-  const onChange = (e) => {
-    const { value } = e.currentTarget;
-    // e.target.value.toLowerCase())
-    console.log(value);
-    setSearchText(value);
+  const onClick = () => {
+    navigate("/searchResult", {
+      state,
+    });
   };
 
   return (
     <form className={cx(styles.searchForm, className)} onSubmit={onSubmit}>
       <input
         type="text"
-        value={searchText}
+        value={state.keyword}
         placeholder={placeholder}
         className={styles.searchInput}
         onChange={onChange}
@@ -41,3 +40,41 @@ const SearchInput = ({ className, placeholder, ...props }) => {
 };
 
 export default memo(SearchInput);
+
+// const SearchInput = ({ className, placeholder, ...props }) => {
+//   const [searchText, setSearchText] = useState("");
+//   const navigate = useNavigate();
+
+//   const onSubmit = (e) => {
+//     e.preventDefault();
+//     // 검색 결과페이지로 이동
+//     navigate(`/searchResult?search=${searchText}`);
+//     // `/searchResult/${searchText}`
+//   };
+
+//   // 검색값
+//   const onChange = (e) => {
+//     const { value } = e.currentTarget;
+//     // e.target.value.toLowerCase())
+//     console.log(value);
+//     setSearchText(value);
+//   };
+
+//   return (
+//     <form className={cx(styles.searchForm, className)} onSubmit={onSubmit}>
+//       <input
+//         type="text"
+//         value={searchText}
+//         placeholder={placeholder}
+//         className={styles.searchInput}
+//         onChange={onChange}
+//         {...props}
+//       />
+//       <button type="submit" className={styles.searchBtn}>
+//         <IconSearch className={styles.iconSearch} />
+//       </button>
+//     </form>
+//   );
+// };
+
+// export default memo(SearchInput);

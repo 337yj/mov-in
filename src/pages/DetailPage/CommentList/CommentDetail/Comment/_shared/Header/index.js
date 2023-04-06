026ccee4ef1f904f785f75d5901ceda246e38a8d
337yj <337yje@gmail.com>
2023-04-06 12:16:22 +0900

    fix/Comment : Fix error commentHeader

--- src/pages/DetailPage/CommentList/CommentDetail/Comment/_shared/Header/index.js
+++ src/pages/DetailPage/CommentList/CommentDetail/Comment/_shared/Header/index.js
@@ -20,7 +20,7 @@ const CommentHeader = ({
 
   return (
     <article className={styles.wrapper}>
-      <figure className={styles.profile}>
+      {/* <figure className={styles.profile}>
 
         <img src={profileImage} alt="" className={styles.profileImage} />
         <figcaption className={styles.username}>{comment.user.name}</figcaption>
@@ -31,7 +31,7 @@ const CommentHeader = ({
         />
         <figcaption className={styles.nickname}>{user.nickname}</figcaption>
       </figure>
-      {/* <output className={className}>평점★{grade?.toFixed(1)}</output> */}
+      <output className={className}>평점★{grade?.toFixed(1)}</output> */}
     </article>
   );
 };

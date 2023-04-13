d5ffb5c73e20b776527d57cdb109d167f47814b0
337yj <337yje@gmail.com>
2023-04-14 00:18:32 +0900

    refactor/DetailPage : Refactor detailpage

--- src/components/Common/Toast/index.js
+++ src/components/Common/Toast/index.js
@@ -1,44 +1,43 @@
 import React, { useState, useEffect } from "react";
-import { CSSTransition } from 'react-transition-group';
+import { CSSTransition } from "react-transition-group";
 import cx from "classnames";
 import styles from "./toast.module.scss";
 import { IconGreenCheck } from "../../../assets/icon";
 import { IconClose } from "../../../assets/icon";
 
 const transitionStyle = {
-  entering : {
+  entering: {
     transform: "translate(-50%, 40px)",
-    transition: "transform 1s ease-in-out"
+    transition: "transform 1s ease-in-out",
   },
 
-  exiting : {
+  exiting: {
     transform: "translate(-50%, -100px)",
-    transition: "transform 1s ease-in-out"
+    transition: "transform 1s ease-in-out",
   },
 
   exited: {
-    transform: 'translate(-50%, -100px)',
-    transition: "transform 1s ease-in-out"
-}
-}
+    transform: "translate(-50%, -100px)",
+    transition: "transform 1s ease-in-out",
+  },
+};
 
 const Toast = ({ className, type, children, func, float, ...props }) => {
   return (
-    <CSSTransition
-      in={float}
-      timeout={3000}
-      unmountOnExit
-    >
+    <CSSTransition in={float} timeout={3000} unmountOnExit>
       {(state) => (
-      <section style={{...transitionStyle[state]}} className={cx(styles.toast, className, styles[type])}>
-        <figure className={cx(styles.circle)}>
-          <IconGreenCheck />
-        </figure>
-        {children}
-        {/* <button onClick={func}>
+        <section
+          style={{ ...transitionStyle[state] }}
+          className={cx(styles.toast, className, styles[type])}
+        >
+          <figure className={cx(styles.circle)}>
+            <IconGreenCheck />
+          </figure>
+          {children}
+          {/* <button onClick={func}>
               <IconClose />
             </button> */}
-      </section>
+        </section>
       )}
     </CSSTransition>
   );
--- src/pages/DetailPage/CommentList/index.js
+++ src/pages/DetailPage/CommentList/index.js
@@ -1,19 +1,38 @@
+import dayjs from "dayjs";
 import React, { useEffect, useState } from "react";
-import { useNavigate, useParams } from "react-router-dom";
-import styles from "./commentList.module.scss";
-import Comment from "./Comment";
+import { useParams } from "react-router-dom";
+import { getMovie } from "../../../api/Movie";
 import { getReviewsMovie } from "../../../api/Review";
+import { formatRuntime } from "../_shared/formatRuntime";
+import Comment from "./Comment";
+import styles from "./commentList.module.scss";
 
-const CommentList = ({ movie, onChangeTab, onChangeSelectedCommentId }) => {
+const CommentList = ({}) => {
   const { id } = useParams();
-  const navigate = useNavigate();
   const [comments, setComments] = useState();
-  console.log(comments);
+  const [movie, setMovie] = useState();
+  const formattedRuntime = formatRuntime(movie?.runtime || 0);
+  console.log(movie);
+
+  const onGetMovieDetail = async () => {
+    try {
+      const response = await getMovie(id);
+      if (response.status === 200) {
+        setMovie(response.data);
+      }
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
   const onGetReviewsMovie = async () => {
     try {
       const response = await getReviewsMovie(id);
       if (response.status === 200) {
-        setComments(response.data);
+        const sortedComments = response.data.sort((a, b) => {
+          return new Date(b.createdAt) - new Date(a.createdAt);
+        });
+        setComments(sortedComments);
       }
     } catch (error) {
       console.log(error);
@@ -22,32 +41,67 @@ const CommentList = ({ movie, onChangeTab, onChangeSelectedCommentId }) => {
 
   useEffect(() => {
     onGetReviewsMovie();
+    onGetMovieDetail();
   }, [id]);
 
   if (!comments) {
     return null;
   }
 
-  const onClickNavigate = (path) => {
-    return () => {
-      navigate(path);
-    };
-  };
   return (
-    <article className={styles.wrapper}>
-      <h2>코멘트</h2>
-      <ul className={styles.commentList}>
-        {comments.map((comment) => (
-          <li key={comment.id} className={styles.comment}>
-            <Comment
-              comment={comment}
-              onClick={onClickNavigate(`/commentDetail/${comment.id}`)}
-            />
-          </li>
-        ))}
-      </ul>
-    </article>
+    <main>
+      <div className={styles.backgroundWrapper}>
+        <img
+          className={styles.backgroundImg}
+          src={movie?.postImage}
+          alt="thumbnail"
+        />
+        <div className={styles.backgroundGradient} />
+      </div>
+      <section className={styles.wrapper}>
+        <article className={styles.infoWrapper}>
+          <div className={styles.title}>
+            <h1>{movie?.title}</h1>
+          </div>
+          <div className={styles.info}>
+            <p>{formattedRuntime}</p>
+            <p>{movie?.genres.map((genre) => genre.name).join(", ")}</p>
+            <p>{dayjs(movie?.releasedAt, "YYYYMMDD").format("YYYY.MM.DD")}</p>
+          </div>
+        </article>
+        <article className={styles.detailInfoWrapper}>
+          <div className={styles.commentWrapper}>
+            <h2>코멘트</h2>
+            <div className={styles.innerWrapper}>
+              <ul className={styles.commentList}>
+                {comments.map((comment) => (
+                  <li key={comment.id} className={styles.comment}>
+                    <Comment
+                      comment={comment}
+                      onGetReviewsMovie={onGetReviewsMovie}
+                    />
+                  </li>
+                ))}
+              </ul>
+            </div>
+          </div>
+        </article>
+      </section>
+    </main>
   );
 };
 
 export default CommentList;
+
+// return (
+//   <article className={styles.wrapper}>
+//     <h2>코멘트</h2>
+//     <ul className={styles.commentList}>
+//       {comments.map((comment) => (
+//         <li key={comment.id} className={styles.comment}>
+//           <Comment comment={comment} onGetReviewsMovie={onGetReviewsMovie} />
+//         </li>
+//       ))}
+//     </ul>
+//   </article>
+// );
